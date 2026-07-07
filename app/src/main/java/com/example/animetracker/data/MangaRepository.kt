2b7595d5567d0826ae99ae28d5622c3c5b01541d
johnrivkw-tech <johnrivkw@gmail.com>
2026-07-07 12:10:34 -0700

    Add MangaRepository for managing manga data

---
+++ app/src/main/java/com/example/animetracker/data/MangaRepository.kt
@@ -0,0 +1,10 @@
+package com.example.animetracker.data
+
+import kotlinx.coroutines.flow.Flow
+
+class MangaRepository(private val dao: MangaDao) {
+    val allManga: Flow<List<MangaEntity>> = dao.getAll()
+
+    suspend fun addManga(manga: MangaEntity) = dao.insert(manga)
+    suspend fun removeManga(manga: MangaEntity) = dao.delete(manga)
+}
