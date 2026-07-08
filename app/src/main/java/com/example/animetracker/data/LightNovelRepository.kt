94698e03770d55498ce3949ed27e838a7209e0bf
johnrivkw-tech <johnrivkw@gmail.com>
2026-07-07 17:34:25 -0700

    Add LightNovelRepository for managing light novels

---
+++ app/src/main/java/com/example/animetracker/data/LightNovelRepository.kt
@@ -0,0 +1,15 @@
+package com.example.animetracker.data
+
+import kotlinx.coroutines.flow.Flow
+
+class LightNovelRepository(private val dao: LightNovelDao) {
+    val allNovels: Flow<List<LightNovelEntity>> = dao.getAll()
+
+    suspend fun addNovel(title: String, uri: String) {
+        dao.insert(LightNovelEntity(title = title, uri = uri))
+    }
+
+    suspend fun removeNovel(novel: LightNovelEntity) {
+        dao.delete(novel)
+    }
+}
