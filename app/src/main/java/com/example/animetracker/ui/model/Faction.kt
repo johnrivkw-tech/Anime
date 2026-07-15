a93bff60234b313018060d45fd5130b266525639
johnrivkw-tech <johnrivkw@gmail.com>
2026-07-14 23:20:32 -0700

    Add Faction enum for One Piece ranks

---
+++ app/src/main/java/com/example/animetracker/ui/model/Faction.kt
@@ -0,0 +1,7 @@
+package com.example.animetracker.ui.model
+
+/** Which side of the One Piece world the user's rank ladder is drawn from. */
+enum class Faction(val displayName: String) {
+    PIRATE("Pirate"),
+    MARINE("Marine")
+}
